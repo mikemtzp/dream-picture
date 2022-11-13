14e37a7c8e3103129539eea6eb8138878bc15758
mikemtzp <mikemp8226@gmail.com>
2022-11-13 13:03:44 -0600

    Add basic UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [query, setQuery] = useState('')
-
-
+  const [query, setQuery] = useState('');
 
   return (
-    <div className="App">
-      <h1>Hi</h1>
+    <div className="main">
+        <h1>Generate an Image using Open AI API</h1>
+
+        <textarea
+          className="input"
+          placeholder="A tall man in a psychedelic endless library, painted by Edvard Munch"
+          onChange={(e) => setQuery(e.target.value)}
+          rows="10"
+          cols="40"
+        />
+        <button>Generate an Image</button>
     </div>
-  )
+  );
 }
 
 export default App
